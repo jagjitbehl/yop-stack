2bf46f9c7c444f7f494e200bb82588d46b93d69a
Phil <Pczek@users.noreply.github.com>
2021-02-17 10:16:22 +0100

    add more info to the staker info

--- src/hooks/useStakerInfo.js
+++ src/hooks/useStakerInfo.js
@@ -11,15 +11,34 @@ export default function useStakerInfo(address) {
                 return
             }
             const stakerInfoResponse = await stakingContract.contract.methods.getStakerInfo(address).call()
-            const rewardResponse = await stakingContract.contract.methods.calculateReward(address).call()
-            setStakerInfo({
-                hasStaked: true,
-                amount: new BigNumber(stakerInfoResponse[0]),
-                stakingTime: parseInt(stakerInfoResponse[1], 10),
-                option: parseInt(stakerInfoResponse[2], 10),
-                rewardTaken: stakerInfoResponse[3],
-                reward: new BigNumber(rewardResponse)
-            })
+
+            if (!new BigNumber(stakerInfoResponse[0]).isZero()) {
+                const [rewardResponse, ...stakedFor] = await Promise.all([
+                    stakingContract.contract.methods.calculateReward(address).call(),
+                    stakingContract.contract.methods.stakedFor1().call(),
+                    stakingContract.contract.methods.stakedFor2().call(),
+                    stakingContract.contract.methods.stakedFor3().call(),
+                ])
+
+                const option = parseInt(stakerInfoResponse[2], 10)
+                const stakingTime = parseInt(stakerInfoResponse[1], 10)
+                const stakedForNumbers = stakedFor.map(value => parseInt(value, 10))
+
+                const startOfStakeMillis = stakingTime * 1000
+                const endOfStakeMillis = (stakedForNumbers[option] + stakingTime) * 1000
+                setStakerInfo({
+                    hasStaked: true,
+                    amount: new BigNumber(stakerInfoResponse[0]),
+                    stakingTime,
+                    startOfStakeMillis,
+                    endOfStakeMillis,
+                    startOfStake: new Date(startOfStakeMillis),
+                    endOfStake: new Date(endOfStakeMillis),
+                    option,
+                    rewardTaken: stakerInfoResponse[3],
+                    reward: new BigNumber(rewardResponse)
+                })
+            }
         }
         fetchStakerInfo()
     }, [setStakerInfo, address])
